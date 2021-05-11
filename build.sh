e9d26d8d6de5180f90da29678561dc61f10e454d
LepkoQQ <lepko.san@gmail.com>
2021-05-11 20:06:57 +0200

    add kubernetes configuration

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
+
+# BUILD AND PUBLISH HUDA POBUDA
+sudo docker build -f hudapobuda/Dockerfile -t hudapobuda:latest ./hudapobuda
+sudo docker tag hudapobuda:latest rg.fr-par.scw.cloud/djnd/hudapobuda:latest
+sudo docker push rg.fr-par.scw.cloud/djnd/hudapobuda:latest
