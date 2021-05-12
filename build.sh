8a93c01775d0c04b0a10d60b8f709ddc2d8e76dd
LepkoQQ <lepko.san@gmail.com>
2021-05-12 14:05:16 +0200

    build docker image from root context

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
 # BUILD AND PUBLISH HUDA POBUDA
-sudo docker build -f hudapobuda/Dockerfile -t hudapobuda:latest ./hudapobuda
+sudo docker build -f hudapobuda/Dockerfile -t hudapobuda:latest .
 sudo docker tag hudapobuda:latest rg.fr-par.scw.cloud/djnd/hudapobuda:latest
 sudo docker push rg.fr-par.scw.cloud/djnd/hudapobuda:latest
