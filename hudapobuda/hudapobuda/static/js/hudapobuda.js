259b5e644df7a168f94108c6306252db21e039f6
LepkoQQ <lepko.san@gmail.com>
2021-05-14 20:11:05 +0200

    add share link functionality

--- hudapobuda/hudapobuda/static/js/hudapobuda.js
+++ hudapobuda/hudapobuda/static/js/hudapobuda.js
@@ -25,23 +25,44 @@
           if (res.ok) {
             return res.text();
           }
-          throw new Error('Response not ok');
+          throw new Error("Response not ok");
         })
         .then((res) => {
-          response.className = 'form-text text-dark';
-          response.textContent = 'Hvala za prijavo!';
+          response.className = "form-text text-dark";
+          response.textContent = "Hvala za prijavo!";
           console.log(res);
         })
         .catch((error) => {
           console.log(error);
-          response.className = 'form-text text-danger';
-          response.textContent = 'Napaka pri prijavi :(';
+          response.className = "form-text text-danger";
+          response.textContent = "Napaka pri prijavi :(";
         })
         .then(() => {
           submitButton.removeAttribute("disabled");
           emailElem.removeAttribute("disabled");
           checkbox.removeAttribute("disabled");
         });
+    });
+  });
+})();
+
+(function () {
+  var shareLinks = document.querySelectorAll(".share .share-box");
+  shareLinks.forEach((shareLink) => {
+    shareLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (event.currentTarget.className.indexOf('isfbbox') != -1) {
+        const url = `https://www.facebook.com/dialog/feed?app_id=301375193309601&redirect_uri=https%3A%2F%2Fhudapobuda.si&link=https%3A%2F%2Fhudapobuda.si&ref=responsive`;
+        window.open(url, '_blank');
+      }
+      if (event.currentTarget.className.indexOf('istwbox') != -1) {
+        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(window.SHARE_TWEET_TEXT + ' https://hudapobuda.si')}`;
+        window.open(url, '_blank');
+      }
+      if (event.currentTarget.className.indexOf('isembox') != -1) {
+        const url = `mailto:?subject=HUDA+POBUDA&body=${encodeURIComponent(window.SHARE_EMAIL_TEXT)}`;
+        window.open(url, '_blank');
+      }
     });
   });
 })();
