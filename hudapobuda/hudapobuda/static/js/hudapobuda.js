4d072ec198ea7e6c517db8c5198deaea4bce0632
LepkoQQ <lepko.san@gmail.com>
2021-05-17 14:44:37 +0200

    fix app id

--- hudapobuda/hudapobuda/static/js/hudapobuda.js
+++ hudapobuda/hudapobuda/static/js/hudapobuda.js
@@ -52,7 +52,7 @@
     shareLink.addEventListener("click", function (event) {
       event.preventDefault();
       if (event.currentTarget.className.indexOf('isfbbox') != -1) {
-        const url = `https://www.facebook.com/dialog/feed?app_id=301375193309601&redirect_uri=https%3A%2F%2Fhudapobuda.si&link=https%3A%2F%2Fhudapobuda.si&ref=responsive`;
+        const url = `https://www.facebook.com/dialog/feed?app_id=831312620809085&redirect_uri=https%3A%2F%2Fhudapobuda.si&link=https%3A%2F%2Fhudapobuda.si&ref=responsive`;
         window.open(url, '_blank');
       }
       if (event.currentTarget.className.indexOf('istwbox') != -1) {
