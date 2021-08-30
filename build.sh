2cf7bd157452cb7c6d3f22ca985f6a989adca667
muki <filipdobranic@gmail.com>
2021-08-30 11:27:14 +0200

    staging initial commit

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
 # BUILD AND PUBLISH HUDA POBUDA
-sudo docker build -f hudapobuda/Dockerfile -t hudapobuda:latest .
-sudo docker tag hudapobuda:latest rg.fr-par.scw.cloud/djnd/hudapobuda:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/hudapobuda:latest
+sudo docker build -f Dockerfile -t hudapobuda-staging:latest .
+sudo docker tag hudapobuda-staging:latest rg.fr-par.scw.cloud/djnd/hudapobuda-staging:latest
+sudo docker push rg.fr-par.scw.cloud/djnd/hudapobuda-staging:latest
